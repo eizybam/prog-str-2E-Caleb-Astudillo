31a373487a48d4ab0b54aa7247f439becffab977
eizybam <eizybam@outlook.com>
2026-01-14 20:22:35 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main() {
         System.out.println("Semana 2");
         System.out.println("Versión base");
+        System.out.println("2 + 2 = 4");
+        System.out.println("5 * 3 = 15");
     }
 }
