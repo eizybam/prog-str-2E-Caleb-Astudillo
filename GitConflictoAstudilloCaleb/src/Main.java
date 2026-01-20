ef0e95c7fa55034423e123b9da0a588cf59ab5e2
eizybam <eizybam@outlook.com>
2026-01-19 23:18:58 -0600

    feat: agregar descuento por umbral

--- GitConflictoAstudilloCaleb/src/Main.java
+++ GitConflictoAstudilloCaleb/src/Main.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-
+    public static final double DESCUENTO = 0.10;
+    public static final double UMBRAL_DESCUENTO = 1000;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         double subtotal = pedirDouble(scanner, "Subtotal: ");
         double total = subtotal;
+        if (total > UMBRAL_DESCUENTO){
+            total = total - (total * DESCUENTO);
+        }
 
         System.out.printf("Total a pagar: %.2f%n", total);
     }
