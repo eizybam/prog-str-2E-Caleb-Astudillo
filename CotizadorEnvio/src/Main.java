91adedf532919d3374f8dd3a524ccf186b88d931
eizybam <eizybam@outlook.com>
2026-02-04 19:53:01 -0600

    Version final

--- CotizadorEnvio/src/Main.java
+++ CotizadorEnvio/src/Main.java
@@ -6,12 +6,30 @@ public class Main {
     static final int MINSERVICIO = 1, MAXSERVICIO = 2;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        ShippingCalculator envio = new ShippingCalculator();
 
-        double pesoKg = leerDoubleEnRango(sc, "Introduce el peso del paquete en Kilogramos: ", MINKG, MAXKG);
-        int distanciaKm = leerIntEnRango(sc, "Introduce la distancia del destino en Kilómetros: ", MINKM, MAXKM);
-        int tipoServicio = leerIntEnRango(sc,"Introduce el tipo de servicio\n1 Estándar\n2 Express: ", MINSERVICIO, MAXSERVICIO);
-        boolean esZonaRemota = leerBoolean(sc, "Es zona remota? Introduce 'true' o 'false': ");
+        envio.pesoKg = leerDoubleEnRango(sc, "Introduce el peso del paquete en Kilogramos: ", MINKG, MAXKG);
+        envio.distanciaKm = leerIntEnRango(sc, "Introduce la distancia del destino en Kilómetros: ", MINKM, MAXKM);
+        envio.tipoServicio = leerIntEnRango(sc,"Introduce el tipo de servicio\n1 Estándar\n2 Express\nQuiero un servicio (1 o 2): ", MINSERVICIO, MAXSERVICIO);
+        envio.zonaRemota = leerBoolean(sc, "Es zona remota? Introduce 'true' o 'false': ");
+
+        envio.subtotal = envio.calcularSubtotal(envio.pesoKg, envio.distanciaKm, envio.tipoServicio, envio.zonaRemota);
+        envio.iva = envio.calcularIVA(envio.subtotal);
+        envio.total = envio.calcularTotal(envio.subtotal, envio.iva);
+
+        imprimirTicket(envio);
+
+
     }
+
+    /**
+     * Lee un dato double, lo limpia de errores y lo regresa
+     * @param sc -> Scanner definido en main
+     * @param msg -> El mensaje a imprimir
+     * @param min -> El valor mínimo permitido
+     * @param max -> El valor máximo permitido
+     * @return double -> El double válido introducido por el usuario
+     */
     public static double leerDoubleEnRango(Scanner sc, String msg, double min, double max){
         double value;
         System.out.print(msg);
@@ -23,7 +41,15 @@ public class Main {
         return value;
     }
 
-    public static int leerIntEnRango(Scanner sc, String msg, double min, double max){
+    /**
+     * Lee un dato int, lo limpia de errores y lo regresa
+     * @param sc -> Scanner definido en main
+     * @param msg -> El mensaje a imprimir
+     * @param min -> El valor mínimo permitido
+     * @param max -> El valor máximo permitido
+     * @return int -> El int válido introducido por el usuario
+     */
+    public static int leerIntEnRango(Scanner sc, String msg, int min, int max){
         int value;
         System.out.print(msg);
         while (!sc.hasNextInt() || (value = sc.nextInt()) < min || value > max) {
@@ -34,6 +60,12 @@ public class Main {
         return value;
     }
 
+    /**
+     * Lee un dato boolean, lo limpia de errores y lo regresa
+     * @param sc -> Scanner definido en main
+     * @param msg -> El mensaje a imprimir
+     * @return boolean -> El boolean válido introducido por el usuario
+     */
     public static boolean leerBoolean(Scanner sc, String msg){
         System.out.print(msg);
         while (!sc.hasNextBoolean()){
@@ -43,8 +75,19 @@ public class Main {
         return sc.nextBoolean();
     }
 
-    public static void imprimirTicket(){
-        System.out.println("---TICKET---");
+    /**
+     * Imprime el ticket de envío con los datos y costos calculados
+     * @param envio -> El objeto ShippingCalculator
+     */
+    public static void imprimirTicket(ShippingCalculator envio){
+        System.out.println("---TICKET DE ENVÍO---");
+        System.out.println("Servicio: " + (envio.tipoServicio));
+        System.out.println("Peso: " + envio.pesoKg + "kg");
+        System.out.println("Distancia: " + envio.distanciaKm + "km");
+        System.out.println("Zona remota? " + envio.zonaRemota);
+        System.out.println("\nSubtotal --> $" + envio.subtotal);
+        System.out.println("IVA 16% --> $" + envio.iva);
+        System.out.println("TOTAL A PAGAR ----> $" + envio.total);
     }
 
 }
--- CotizadorEnvio/src/ShippingCalculator.java
+++ CotizadorEnvio/src/ShippingCalculator.java
@@ -1,13 +1,50 @@
 public class ShippingCalculator {
-    private double calcularSubtotal(double pesoKg, int distanciaKm, int tipoServicio, boolean zonaRemota){
-        double subtotal = ()
+    double subtotal, total, iva, pesoKg;
+    int distanciaKm, tipoServicio;
+    boolean zonaRemota;
+
+    /**
+     * Calcula el subtotal del envío basado en parametros dados
+     * @param pesoKg -> El peso del paquete en kg
+     * @param distanciaKm -> La distancia a recorrer en km
+     * @param tipoServicio -> El tipo de servicio (1 para estándar, 2 para express)
+     * @param zonaRemota -> Si es zona remota o no (true o false)
+     * @return double -> El subtotal calculado
+     */
+    public double calcularSubtotal(double pesoKg, int distanciaKm, int tipoServicio, boolean zonaRemota){
+        double subtotal = (tipoServicio == 1) ? 50.0 : 90.0;
+        subtotal += 12 * pesoKg;
+        if (distanciaKm <= 50) {
+            subtotal += 20;
+        }
+        else if (distanciaKm <= 200) {
+            subtotal += 60;
+        }
+        else {
+            subtotal += 120;
+        }
+
+        subtotal += (zonaRemota) ? subtotal * 0.10 : 0;
+
+        return subtotal;
     }
 
-    private double calcularIVA(double subtotal){
-
+    /**
+     * Calcula el IVA basado en el subtotal dado
+     * @param subtotal -> El subtotal del envío
+     * @return double -> El IVA calculado
+     */
+    public double calcularIVA(double subtotal){
+        return subtotal * 0.16;
     }
 
-    private double calcularTotal(double subtotal, double iva){
-
+    /**
+     * Calcula el total del envío sumando el subtotal y el IVA
+     * @param subtotal -> El subtotal del envío
+     * @param iva -> El IVA del envío
+     * @return double -> El total calculado
+     */
+    public double calcularTotal(double subtotal, double iva){
+        return subtotal + iva;
     }
 }
