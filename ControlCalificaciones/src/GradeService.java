1a00731d31e8e070c88ec675e080a77e90f68940
eizybam <eizybam@outlook.com>
2026-02-06 22:32:49 -0600

    Version final ControlCalificaciones

--- ControlCalificaciones/src/GradeService.java
+++ ControlCalificaciones/src/GradeService.java
@@ -1,22 +1,78 @@
 public class GradeService {
-    private double average;
-    double finalGrade;
-    double status;
-    double a, b, c;
+    private double average, finalGrade;
+    private final int ATTENDANCE;
+    private String state, name;
+    private final boolean FINAL_PROJECT_IS_APPROVED;
+    double[] grades;
 
-    GradeService(double p1, double p2, double p3){
-        this.a = p1;
-        this.b = p2;
-        this.c = p3;
+    /**
+     * Constructor de la clase GradeService
+     * @param name -> Nombre del alumno
+     * @param grades -> Arreglo de calificaciones parciales
+     * @param attendance -> Porcentaje de asistencia
+     * @param finalProjectIsApproved -> Booleano que indica si el proyecto final fue aprobado o no
+     */
+    GradeService(String name, double[] grades, int attendance, boolean finalProjectIsApproved){
+        this.name = name;
+        this.grades = grades;
+        this.ATTENDANCE = attendance;
+        this.FINAL_PROJECT_IS_APPROVED = finalProjectIsApproved;
+        this.finalGrade = -1;
     }
 
-    public void setAverage(){
-        this.average = (this.a + this.b + this.c) / 3;
+    /**
+     * Calcula el promedio de las calificaciones parciales y lo asigna a la variable average
+     */
+    private void setAverage(){
+        for (double grade : this.grades) {
+            this.average += grade;
+        }
+        this.average /= this.grades.length;
     }
 
-    public double getAverage(){
-        return this.average;
+    /**
+     * Calcula la calificación final basada en el promedio y la asistencia, y lo asigna a la variable finalGrade
+     */
+    private void setFinalGrade(){
+        this.finalGrade = (this.average * 0.7) + (this.ATTENDANCE * 0.3);
     }
 
+    /**
+     * Determina el estado del alumno basado en la asistencia, el proyecto final y la calificación final, y lo asigna a la variable state
+     */
+    public void setState(){
+        this.setAverage();
+        if (this.ATTENDANCE < 80) {
+            this.state = "REPROBADO por asistencia";
+        }
+
+        else if (!this.FINAL_PROJECT_IS_APPROVED){
+            this.state = "REPROBADO por proyecto";
+        }
+
+        else {
+            this.setFinalGrade();
+            if (this.finalGrade >= 70) {
+                this.state = "APROBADO";
+            }
+            else {
+                this.state = "REPROBADO por calificación";
+            }
+        }
+
+    }
+
+    /**
+     * Imprime un reporte con el nombre del alumno, sus calificaciones parciales, su promedio, su asistencia, si entregó el proyecto final, su calificación final y su estado
+     */
+    public void printReport(){
+        System.out.println("\n-- REPORTE DE CALIFICACIONES del alumno: " + this.name + " --");
+        System.out.println("Calificaciones parciales: " + this.grades[0] + ", " + this.grades[1] + ", " + this.grades[2]);
+        System.out.println("- PROMEDIO: " + this.average);
+        System.out.println("- ASISTENCIA: " + this.ATTENDANCE + "%");
+        System.out.println("- Entregó proyecto final: " + ((this.FINAL_PROJECT_IS_APPROVED) ? "Sí": "No"));
+        System.out.println(("---> CALIFICACIÓN FINAL: " + ((this.finalGrade >= 0) ? this.finalGrade: "No aplica").toString()));
+        System.out.println("----> ESTADO: " + this.state);
+    }
 
 }
--- ControlCalificaciones/src/Main.java
+++ ControlCalificaciones/src/Main.java
@@ -1,6 +1,92 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        GradeService calificaciones = new GradeService(8, 9.2, 7);
-        calificaciones.setAverage();
+        Scanner sc = new Scanner(System.in);
+        double[] grades = new double[3];
+
+        String name = getValidText(sc, "Introduce el nombre: ");
+
+        int counter = 0;
+        while (counter < 3){
+            grades[counter] = getDoubleInRange(sc, "Introduce la calificación " + (counter + 1) + " (0 - 100): ", 0, 100);
+            counter++;
+        }
+        int attendance = getIntInRange(sc, "Introduce el porcentaje de asistencia (0-100): ", 0, 100);
+        boolean finalProjectIsApproved = readBoolean(sc, "¿Entregó el proyecto final? (true/false): ");
+
+        GradeService gradeservice = new GradeService(name, grades, attendance, finalProjectIsApproved);
+        gradeservice.setState();
+        gradeservice.printReport();
     }
+
+    /**
+     * Lee un texto, se asegura que no esté vacío y lo regresa
+     * @param sc -> Scanner definido en main
+     * @param msg -> El mensaje a imprimir
+     * @return String -> El texto válido introducido por el usuario
+     */
+    public static String getValidText(Scanner sc, String msg){
+        System.out.print(msg);
+        String input = sc.nextLine();
+        while (input.trim().isEmpty()){
+            System.out.print("Introduce un texto válido: ");
+            input = sc.nextLine();
+        }
+        return input;
+    }
+
+    /**
+     * Lee un dato double, lo limpia de errores y lo regresa
+     * @param sc -> Scanner definido en main
+     * @param msg -> El mensaje a imprimir
+     * @param min -> El valor mínimo permitido
+     * @param max -> El valor máximo permitido
+     * @return double -> El double válido introducido por el usuario
+     */
+    public static double getDoubleInRange(Scanner sc, String msg, double min, double max){
+        double value;
+        System.out.print(msg);
+        while (!sc.hasNextDouble() || (value = sc.nextDouble()) < min || value > max) {
+            System.out.print("No es double válido o está fuera del rango, pon otro: ");
+            sc.nextLine();
+        }
+        sc.nextLine();
+        return value;
+    }
+
+    /**
+     * Lee un dato int, lo limpia de errores y lo regresa
+     * @param sc -> Scanner definido en main
+     * @param msg -> El mensaje a imprimir
+     * @param min -> El valor mínimo permitido
+     * @param max -> El valor máximo permitido
+     * @return int -> El int válido introducido por el usuario
+     */
+    public static int getIntInRange(Scanner sc, String msg, int min, int max){
+        int value;
+        System.out.print(msg);
+        while (!sc.hasNextInt() || (value = sc.nextInt()) < min || value > max) {
+            System.out.print("No es entero válido o está fuera del rango, pon otro: ");
+            sc.nextLine();
+        }
+        sc.nextLine();
+        return value;
+    }
+
+    /**
+     * Lee un dato boolean, lo limpia de errores y lo regresa
+     * @param sc -> Scanner definido en main
+     * @param msg -> El mensaje a imprimir
+     * @return boolean -> El boolean válido introducido por el usuario
+     */
+    public static boolean readBoolean(Scanner sc, String msg){
+        System.out.print(msg);
+        while (!sc.hasNextBoolean()){
+            System.out.println("Ingresa solo 'true' o 'false': ");
+            sc.nextLine();
+        }
+        return sc.nextBoolean();
+    }
+
 }
